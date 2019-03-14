f1e69731be38f35872f156b503b165e51a9c2865
alex <huppertam@gmail.com>
2019-03-14 10:48:44 -0400

    Redesign of dispatcher to get more intuitive behavior

--- src/Redux.purs
+++ src/Redux.purs
@@ -10,29 +10,24 @@ module Redux
 
 import Prelude
 
-import Control.Monad.Rec.Class (Step(..), tailRecM)
-import Data.Array as Array
+import Data.Foldable (traverse_)
 import Data.Map (Map)
 import Data.Map as Map
-import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Ref (Ref)
 import Effect.Ref as Ref
-import MutableQueue (MQueue)
-import MutableQueue as MQ
 
 type Reducer state action = state -> action -> state
 
 newtype ReduxStore state action =
   ReduxStore { reducer :: Reducer state action
              , state :: Ref state
-             , subscribers :: Ref (Map Int (state -> Effect Unit))
+             , subscribers :: Ref (Map Int (Effect Unit))
              , nextSubId :: Ref Int
-             , dispatchQueue :: MQueue (Effect Unit)
              , preDispatchHook :: state -> action -> Effect Unit
              }
 
-subscribe :: forall s a. ReduxStore s a -> (s -> Effect Unit) -> Effect (Effect Unit)
+subscribe :: forall s a. ReduxStore s a -> Effect Unit -> Effect (Effect Unit)
 subscribe (ReduxStore store) callback = do
   subscriberMap <- Ref.read store.subscribers
   subId <- Ref.read store.nextSubId
@@ -46,28 +41,16 @@ dispatch :: forall s a. ReduxStore s a -> a -> Effect Unit
 dispatch (ReduxStore store) action = do
   priorState <- Ref.read store.state
   store.preDispatchHook priorState action
-  newState <- Ref.modify (flip store.reducer action) store.state
+  Ref.modify_ (flip store.reducer action) store.state
   subscribers <- Ref.read store.subscribers
-  let callbacks = Array.fromFoldable $
-                  map (\callback -> callback newState) (Map.values subscribers)
-  MQ.enqueueAll store.dispatchQueue callbacks
-  tailRecM processQueueStep unit
-
+  notifySubscribers subscribers
   where
-    processQueueStep :: Unit -> Effect (Step Unit Unit)
-    processQueueStep _ = do
-      maybeNext <- MQ.dequeue store.dispatchQueue
-      case maybeNext of
-        Just notify -> notify *> loop
-        Nothing -> done
-    loop = pure (Loop unit)
-    done = pure (Done unit)
+    notifySubscribers subs = traverse_ identity $ Map.values subs
 
 createStore' :: forall s a. (s -> a -> Effect Unit) -> Reducer s a -> s -> Effect (ReduxStore s a)
 createStore' preDispatchHook reducer initialState = do
   stateRef <- Ref.new initialState
   subscribers <- Ref.new Map.empty
-  dispatchQueue <- MQ.newQueue
   nextSubId <- Ref.new 0
   pure $
     ReduxStore
@@ -75,7 +58,6 @@ createStore' preDispatchHook reducer initialState = do
     , state: stateRef
     , subscribers
     , nextSubId
-    , dispatchQueue
     , preDispatchHook
     }
 
--- test/Main.purs
+++ test/Main.purs
@@ -2,25 +2,17 @@ module Test.Main where
 
 import Prelude
 
-import Data.Enum (enumFromTo)
-import Data.Foldable (find, foldl, for_)
-import Data.List (List(..))
-import Data.List as List
-import Data.Maybe (Maybe(..))
-import Data.NonEmpty (NonEmpty(..))
-import Data.String.CodeUnits (fromCharArray)
-import Data.Traversable (for)
-import Data.Tuple (Tuple(..))
+import Data.Array as Array
+import Data.Foldable (foldl, for_)
 import Effect (Effect)
 import Effect.Class (liftEffect)
-import Effect.Ref (Ref)
 import Effect.Ref as Ref
 import Effect.Unsafe (unsafePerformEffect)
 import MutableQueueSpec as MutableQueueSpec
 import Redux as Redux
-import Test.QuickCheck (class Arbitrary, Result(..), arbitrary, quickCheck, quickCheckGen')
-import Test.QuickCheck.Gen (Gen, arrayOf, elements, frequency, oneOf)
+import Test.QuickCheck (quickCheck, (==?))
 import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (run)
 
@@ -36,62 +28,24 @@ main = run [consoleReporter] do
               for_ actions (Redux.dispatch store)
               Redux.getState store
             stateAfterFold = foldl reducer initialState actions
-         in stateAfterDispatches == stateAfterFold
+         in stateAfterDispatches ==? stateAfterFold
   describe "Subscribers" do
-    it "All subscribers are notified about each iteration of the state in the same order" do
-      liftEffect $ quickCheckGen' 1000 do
-        (initialState :: String) <- genStr
-        (actions :: Array String) <- arrayOf genStr
-        (subscribers :: Array SubCallbackSpec) <- arbitrary
-        let reducer = (<>)
-            {states,subscriberSeenStates} = unsafePerformEffect do
-              statesRef <- Ref.new []
-              store <- Redux.createStore reducer initialState
-              let dispatch = \action -> do
-                    currState <- Redux.getState store
-                    let nextState = reducer currState action
-                    Ref.modify_ (_ <> [nextState]) statesRef
-                    Redux.dispatch store action
-              subscriberSeenStateRefs :: Array (Ref (Array String)) <- for subscribers \subSpec -> do
-                seenStatesRef <- Ref.new []
-                remainingActionsRef <- Ref.new Nil
-                case subSpec of
-                  Dispatch actionsToDispatch -> Ref.write (List.fromFoldable actionsToDispatch) remainingActionsRef
-                  _ -> pure unit
-                void $ Redux.subscribe store \s -> do
-                  Ref.modify_ (_ <> [s]) seenStatesRef
-                  remainingActions <- Ref.read remainingActionsRef
-                  case List.uncons remainingActions of
-                    Just {head,tail} -> do
-                      Ref.write tail remainingActionsRef
-                      dispatch head
-                    Nothing -> pure unit
-                pure seenStatesRef
-              for_ actions dispatch
-              states <- Ref.read statesRef
-              subscriberSeenStates <- for subscriberSeenStateRefs Ref.read
-              pure {states, subscriberSeenStates}
-        case find (_ /= states) subscriberSeenStates of
-          Just _ -> do
-            let msg = show states <> " /= " <> show subscriberSeenStates
-            pure $ Failed msg
-          Nothing -> pure Success
-
--- Describes how a subscriber callback behaves.
--- It either does nothing, or dispatches an action.
-data SubCallbackSpec = NoOp | Dispatch (Array String)
-
-instance arbSubCallbackSpec :: Arbitrary SubCallbackSpec where
-  arbitrary = frequency $
-              NonEmpty
-              (Tuple 10.0 (pure NoOp))
-              (List.fromFoldable [ Tuple 4.0 (Dispatch <$> (arrayOf genStr))])
-
-genStr :: Gen String
-genStr = fromCharArray <$> arrayOf genLetter
-
-genLetter :: Gen Char
-genLetter = do
-  let lower = elements $ NonEmpty 'a' (enumFromTo 'b' 'z')
-      upper = elements $ NonEmpty 'A' (enumFromTo 'B' 'Z')
-  oneOf $ NonEmpty lower [upper]
+    it "Subscribers are notified" do
+      let reducer = Array.snoc
+      redux <- liftEffect $ Redux.createStore reducer []
+      sub1 <- liftEffect $ Ref.new []
+      void $ liftEffect $ Redux.subscribe redux do
+        s <- Redux.getState redux
+        Ref.write s sub1
+      sub2 <- liftEffect $ Ref.new []
+      void $ liftEffect $ Redux.subscribe redux do
+        s <- Redux.getState redux
+        Ref.write s sub2
+      liftEffect do
+        Redux.dispatch redux 15
+        Redux.dispatch redux 10
+        Redux.dispatch redux 20
+      sub1State <- liftEffect $ Ref.read sub1
+      sub2State <- liftEffect $ Ref.read sub2
+      sub1State `shouldEqual` [15,10,20]
+      sub2State `shouldEqual` [15,10,20]
